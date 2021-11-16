e8d379d30bf46a4e8587734fda3220bd2cb775fe
xxsimoxx <simone@gieffeedizioni.it>
2021-11-16 13:42:04 +0100

    WPCS 2

--- js/index.php
+++ js/index.php
@@ -1,3 +1,1 @@
 <?php
-// nothing to see here
-?>
--- vars.php
+++ vars.php
@@ -64,14 +64,14 @@ function vars_save_security_settings( $admin_referer ) {
 	if ( isset( $_POST['doeverywhere'] ) || isset( $_POST['cleanup'] ) || isset( $_POST['whocanedit'] ) ) {
 		check_admin_referer( $admin_referer );
 		if ( isset( $_POST['doeverywhere'] ) ) {
-			update_option( 'vars-doeverywhere', 1 );
+			update_option( 'vars-doeverywhere', '1' );
 		} else {
-			update_option( 'vars-doeverywhere', 0 );
+			update_option( 'vars-doeverywhere', '0' );
 		};
 		if ( isset( $_POST['cleanup'] ) ) {
-			update_option( 'vars-cleanup', 1 );
+			update_option( 'vars-cleanup', '1' );
 		} else {
-			update_option( 'vars-cleanup', 0 );
+			update_option( 'vars-cleanup', '0' );
 		};
 		if ( isset( $_POST['whocanedit'] ) ) {
 			if ( current_user_can( $_POST['whocanedit'] ) ) {
@@ -86,22 +86,10 @@ function vars_save_security_settings( $admin_referer ) {
 };
 
 function vars_render_security_settings( $errors ) {
-?>
-	<input type="checkbox" name="doeverywhere" class="doeverywhere"
-	<?php
-	if ( 1 === get_option( 'vars-doeverywhere' ) ) {
-		echo "checked='checked'";
-	}
 	?>
-	>
+	<input type="checkbox" name="doeverywhere" value="1" class="doeverywhere" <?php checked( get_option( 'vars-doeverywhere', '0' ), '1' ); ?>>
 	<?php _e( 'Do shortcodes anywhere.', 'vars' ); ?> </input><br>
-	<input type="checkbox" name="cleanup" class="cleanup"
-	<?php
-	if ( 1 === get_option( 'vars-cleanup' ) ) {
-		echo "checked='checked'";
-	}
-	?>
-	>
+	<input type="checkbox" name="cleanup" value="1" class="cleanup" <?php checked( get_option( 'vars-cleanup', '0' ), '1' ); ?>>
 	<?php _e( 'Delete plugin data at uninstall.', 'vars' ); ?></input><br>
 	<?php _e( 'User capability requested to edit vars:', 'vars' ); ?>
 	<input type="text" name="whocanedit" class="whocanedit" value="<?php echo get_option( 'vars-whocanedit' ); ?>">
@@ -115,13 +103,13 @@ function vars_render_security_settings( $errors ) {
 		$meta = get_user_meta( $user_id->ID );
 		if ( user_can( $user_id->ID, get_option( 'vars-whocanedit' ) ) ) {
 			$count++;
-			$userlist            .= $meta['nickname'][0] . ', '';
-		};
+			$userlist .= $meta['nickname'][0] . ', ';
+		}
 	}
-	echo "<p>";
+	echo '<p>';
 	/* translators: 1 is the number of user. 2 is the list of users */
-	printf(_n('%1$d user has <i>%3$s</i> capability and so can change vars: %2$s.', '%1$d users have <i>%3$s</i> capability and so can change vars: %2$s.', $count, 'vars'), $count, rtrim( $userlist, ', ' ), get_option( 'vars-whocanedit' ) );
-	echo "</p><hr>";
+	printf( _n( '%1$d user has <i>%3$s</i> capability and so can change vars: %2$s.', '%1$d users have <i>%3$s</i> capability and so can change vars: %2$s.', $count, 'vars' ), $count, rtrim( $userlist, ', ' ), get_option( 'vars-whocanedit' ) );
+	echo '</p><hr>';
 };
 
 /*
@@ -130,15 +118,17 @@ function vars_render_security_settings( $errors ) {
  *
  */
 add_action( 'admin_footer', 'vars_admin_script' );
-function vars_admin_script( ) {
+function vars_admin_script() {
 	$screen = get_current_screen();
-	if ( 'tools_page_vars-options' === $screen->id ){
-		wp_enqueue_script( 'vars_admin', plugins_url( 'js/vars-admin.js', __FILE__ ), array('jquery'), '1.0' );
-		wp_localize_script( 'vars_admin', 'objectL10n',
+	if ( 'tools_page_vars-options' === $screen->id ) {
+		wp_enqueue_script( 'vars_admin', plugins_url( 'js/vars-admin.js', __FILE__ ), array( 'jquery' ), '1.0' );
+		wp_localize_script(
+			'vars_admin',
+			'objectL10n',
 			array(
-				'save'     => __( 'Save', 'vars' ),
-				'name'     => __( 'name', 'vars' ),
-				'content'  => __( 'content', 'vars' ),
+				'save'    => __( 'Save', 'vars' ),
+				'name'    => __( 'name', 'vars' ),
+				'content' => __( 'content', 'vars' ),
 			)
 		);
 	}
@@ -147,10 +137,10 @@ function vars_admin_script( ) {
 add_action( 'admin_menu', 'vars_create_menu' );
 function vars_create_menu() {
 	if ( current_user_can( get_option( 'vars-whocanedit' ) ) ) {
-		$page=add_submenu_page(
+		$page = add_submenu_page(
 			'tools.php',
-			__('SETTINGS_PAGE_TITLE', 'vars'),
-			__('SETTINGS_PAGE_NAME', 'vars'),
+			__( 'SETTINGS_PAGE_TITLE', 'vars' ),
+			__( 'SETTINGS_PAGE_NAME', 'vars' ),
 			get_option( 'vars-whocanedit' ),
 			'vars-options',
 			'vars_settings_page'
@@ -162,10 +152,10 @@ function vars_settings_page() {
 	if ( ! current_user_can( get_option( 'vars-whocanedit' ) ) ) {
 		exit;
 	}
-	if ( isset( $_POST["allvars"] ) || isset( $_POST["doeverywhere"] ) || isset( $_POST["cleanup"] ) || isset( $_POST["whocanedit"] )){
+	if ( isset( $_POST['allvars'] ) || isset( $_POST['doeverywhere'] ) || isset( $_POST['cleanup'] ) || isset( $_POST['whocanedit'] ) ) {
 		check_admin_referer( 'vars-admin' );
-		parse_str( $_POST["allvars"], $testvars );
-		update_option( 'vars-vars', $_POST["allvars"] );
+		parse_str( $_POST['allvars'], $testvars );
+		update_option( 'vars-vars', $_POST['allvars'] );
 		if ( current_user_can( 'manage_options' ) ) {
 			$cap_error = vars_save_security_settings( 'vars-admin' );
 		};
@@ -174,7 +164,7 @@ function vars_settings_page() {
 		parse_str( $coded_options, $testvars );
 	};
 	// text about plugin usage I prefer storing in the translations
-	$header = __("HEADERTEXT" , 'vars' );
+	$header = __( 'HEADERTEXT', 'vars' );
 	?>
 	<style>
 		.form-table {
@@ -194,17 +184,17 @@ function vars_settings_page() {
 	<hr>
 	<form method="POST" id="vars-form"  >
 	<?php
-	if ( current_user_can('manage_options') && ! function_exists( '\add_security_page' ) ){
+	if ( current_user_can( 'manage_options' ) && ! function_exists( '\add_security_page' ) ) {
 		vars_render_security_settings( $cap_error );
 	};
-	if ( current_user_can('manage_options') && function_exists( '\add_security_page' ) ) {
+	if ( current_user_can( 'manage_options' ) && function_exists( '\add_security_page' ) ) {
 		$security_link = '<a href="' . admin_url( 'security.php?page=vars' ) . '" title="' . __( 'Security settings', 'vars' ) . '"><i class="dashicons-before dashicons-shield">';
-		echo $security_link . __( "Edit security settings",'vars') . '</i></a><hr>';
+		echo $security_link . __( 'Edit security settings', 'vars' ) . '</i></a><hr>';
 	};
 	?>
 		<table class="form-table">
 	<?php
-	foreach ( $testvars as $key => $value ){
+	foreach ( $testvars as $key => $value ) {
 		echo '<tr valign="top" class="vars-keyvalue"><td ><input type="text" size="20" class="vars-key" value="' . $key . '" /></td>';
 		echo '<td ><input type="text" size="100" class="vars-value" value="' . htmlspecialchars( $value ) . '" /></td><td><a class="dashicons dashicons-trash vars-delete"></a></td></tr>';
 	}
@@ -227,17 +217,17 @@ add_action( 'admin_menu', 'vars_create_security_menu' );
 function vars_create_security_menu() {
 	if ( function_exists( '\add_security_page' ) ) {
 		add_security_page(
-			__('SECURITY_SETTINGS_PAGE_TITLE', 'vars'),
-			__('SECURITY_SETTINGS_PAGE_NAME', 'vars'),
+			__( 'SECURITY_SETTINGS_PAGE_TITLE', 'vars' ),
+			__( 'SECURITY_SETTINGS_PAGE_NAME', 'vars' ),
 			dirname( plugin_basename( __FILE__ ) ),
-			'vars'
+			'vars_security_page'
 		);
 	};
 };
 
 function vars_security_page() {
-	$cap_error = "";
-	if ( isset( $_POST["doeverywhere"] ) || isset( $_POST["cleanup"] ) || isset( $_POST["whocanedit"] )){
+	$cap_error = '';
+	if ( isset( $_POST['doeverywhere'] ) || isset( $_POST['cleanup'] ) || isset( $_POST['whocanedit'] ) ) {
 		check_admin_referer( 'vars-security' );
 		$cap_error = vars_save_security_settings( 'vars-security' );
 	};
@@ -268,24 +258,24 @@ function vars_security_page() {
  * shortcode section
  *
  */
-add_shortcode('vars', 'cpv');
+add_shortcode( 'vars', 'cpv' );
 function cpv( $atts, $content = null ) {
 	$coded_options = get_option( 'vars-vars' );
 	parse_str( $coded_options, $testvars );
-	if ( isset( $testvars[$content] ) ){
-		$prefilter_retval = $testvars[$content];
-		$filtered_retval = apply_filters( 'vars_output', $prefilter_retval );
+	if ( isset( $testvars[ $content ] ) ) {
+		$prefilter_retval = $testvars[ $content ];
+		$filtered_retval  = apply_filters( 'vars_output', $prefilter_retval );
 		return $filtered_retval;
 	} elseif ( current_user_can( get_option( 'vars-whocanedit' ) ) ) {
 		$url = admin_url( 'tools.php?page=vars-options' );
 		/* translators: 1 is the var not defined. 2 is the url of the admin page */
-		return sprintf ( __('%1$s is not defined. Define it <a href="%2$s">here</a>. (only you can see this message)', 'vars'), $content, $url );
+		return sprintf( __( '%1$s is not defined. Define it <a href="%2$s">here</a>. (only you can see this message)', 'vars' ), $content, $url );
 	} else {
-		return "";
+		return '';
 	}
 }
 
-function vars_do ( $var ) {
+function vars_do( $var ) {
 	return cpv( '', $var );
 };
 
@@ -294,18 +284,17 @@ function vars_do ( $var ) {
  * do shortcodes everywhere section
  *
  */
-if ( 1 == get_option( 'vars-doeverywhere' ) ){
-	$vars_shortcodeseverywhere_pryority = 10;
-	$tags = [
+if ( '1' === get_option( 'vars-doeverywhere', '0' ) ) {
+	$tags = array(
 		'single_post_title',
 		'the_title',
 		'widget_text',
 		'widget_title',
 		'bloginfo',
-		'get_post_metadata'
-	];
-	foreach ( $tags as $tag ){
-		add_filter( $tag, 'do_shortcode', $vars_shortcodeseverywhere_pryority );
+		'get_post_metadata',
+	);
+	foreach ( $tags as $tag ) {
+		add_filter( $tag, 'do_shortcode', 10 );
 	}
 }
 
@@ -314,41 +303,41 @@ if ( 1 == get_option( 'vars-doeverywhere' ) ){
  * Add a menu to mce
  *
  */
-foreach ( array('post.php','post-new.php') as $hook ) {
+foreach ( array( 'post.php', 'post-new.php' ) as $hook ) {
 	add_action( "admin_head-$hook", 'vars_admin_head' );
 }
 
 function vars_admin_head() {
 	$coded_options = get_option( 'vars-vars' );
 	parse_str( $coded_options, $testvars );
-	$vars_dynamic_mce = "";
-	$vars_dynamic_mce5 = "";
-	foreach ( $testvars as $var => $value){
-		if ( strlen( $value ) <= 10 ){
+	$vars_dynamic_mce  = '';
+	$vars_dynamic_mce5 = '';
+	foreach ( $testvars as $var => $value ) {
+		if ( strlen( $value ) <= 10 ) {
 			$example_data = wp_strip_all_tags( $value );
 		} else {
-			$example_data = substr( wp_strip_all_tags( $value ), 0, 12) . "...";
-		};
-		$example_data = ' (' . $example_data . ')';
-		$vars_dynamic_mce .=
+			$example_data = substr( wp_strip_all_tags( $value ), 0, 12 ) . '...';
+		}
+		$example_data       = ' (' . $example_data . ')';
+		$vars_dynamic_mce  .=
 			'{text: "' . $var . $example_data . '",onclick: function() {tinymce.activeEditor.insertContent("[vars]' . $var . '[/vars]"); }},';
 		$vars_dynamic_mce5 .=
 			'{type:"menuitem", text: "' . $var . $example_data . '",onAction: function() {tinymce.activeEditor.insertContent("[vars]' . $var . '[/vars]"); }},';
 
-	};
-	$vars_dynamic_mce = '$vars_dynmenu=[' . $vars_dynamic_mce . ']';
+	}
+	$vars_dynamic_mce  = '$vars_dynmenu=[' . $vars_dynamic_mce . ']';
 	$vars_dynamic_mce5 = '$vars_dynmenu=[' . $vars_dynamic_mce5 . ']';
 	echo '<script type="text/javascript">';
 	echo vars_is_mce_5() ? $vars_dynamic_mce5 : $vars_dynamic_mce;
 	echo '</script>';
 }
 
-add_action('admin_head', 'vars_add_mce_menu');
+add_action( 'admin_head', 'vars_add_mce_menu' );
 function vars_add_mce_menu() {
-	if ( !current_user_can( 'edit_posts' ) &&  !current_user_can( 'edit_pages' ) ) {
+	if ( ! current_user_can( 'edit_posts' ) && ! current_user_can( 'edit_pages' ) ) {
 		return;
 	}
-	if ( 'true' == get_user_option( 'rich_editing' ) ) {
+	if ( 'true' === get_user_option( 'rich_editing' ) ) {
 		add_filter( 'mce_external_plugins', 'vars_add_tinymce_plugin' );
 		add_filter( 'mce_buttons', 'vars_register_mce_menu' );
 	}
@@ -362,11 +351,11 @@ function vars_register_mce_menu( $buttons ) {
 
 function vars_is_mce_5() {
 	global $tinymce_version;
-	return isset($tinymce_version) && substr($tinymce_version, 0, 1) === '5';
+	return isset( $tinymce_version ) && substr( $tinymce_version, 0, 1 ) === '5';
 }
 
 function vars_add_tinymce_plugin( $plugin_array ) {
-	$js = vars_is_mce_5() ? 'js/vars-mce-menu-5.js' : 'js/vars-mce-menu.js';
+	$js                            = vars_is_mce_5() ? 'js/vars-mce-menu-5.js' : 'js/vars-mce-menu.js';
 	$plugin_array['vars_mce_menu'] = plugins_url( $js, __FILE__ );
 	return $plugin_array;
 }
@@ -377,8 +366,8 @@ function vars_add_tinymce_plugin( $plugin_array ) {
  *
  */
 register_uninstall_hook( __FILE__, 'vars_cleanup' );
-function vars_cleanup () {
-	if ( 1 == get_option( 'vars-cleanup' ) ){
+function vars_cleanup() {
+	if ( '1' === get_option( 'vars-cleanup', '0' ) ) {
 		delete_option( 'vars-cleanup' );
 		delete_option( 'vars-doeverywhere' );
 		delete_option( 'vars-vars' );
@@ -389,7 +378,7 @@ function vars_cleanup () {
 register_activation_hook( __FILE__, 'vars_activate' );
 function vars_activate() {
 	// If permission options not set, let admin make changes
-	if ( false === get_option( 'vars-whocanedit' ) ){
-		update_option ( 'vars-whocanedit', 'manage_options');
-	};
+	if ( false === get_option( 'vars-whocanedit' ) ) {
+		update_option( 'vars-whocanedit', 'manage_options' );
+	}
 }
